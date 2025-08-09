0faa9ab13faa59e41fdcff99f35c816c6ad187f1
UisekStudent <nicolas.haro@uisek.edu.ec>
2025-08-08 20:28:13 -0500

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")  // ¡IMPORTANTE!
+    implementation("androidx.appcompat:appcompat:1.6.1") 
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
